45e0df0907deb01f17d2c30b5c18e90a45b6802f
AnshitaGupta20 <anshita.educlouds@gmail.com>
2023-08-20 16:23:08 +0530

    React Native Basic

---
+++ ReactNative/AwesomeProject/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  const decrementCount = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.counterText}>Counter: {count}</Text>
+      <Button title="Increment" onPress={incrementCount}/>
+      <Button title="Decrement" onPress={decrementCount} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor:"red"
+  },
+  counterText: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+});
